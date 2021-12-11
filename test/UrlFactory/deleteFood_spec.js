3c8d5d0d9711a4c812cc68bfa539650ce1b08908
Maxim Katargin <maxim.katargin@kuehne-nagel.com>
2021-12-11 13:21:45 +0200

    Added unit test for deleteFood

---
+++ test/UrlFactory/deleteFood_spec.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const expect = require('chai').expect;
+const UrlFactory = require("../../UrlFactory");
+
+describe('UrlFactory.deleteActivity', () => {
+
+  it('should throw if no argument passed', done => {
+    expect(() => UrlFactory.deleteFood()).to.throw("No data specified");
+    done();
+  });
+
+  it('should throw if no logId passed', done => {
+    expect(() => UrlFactory.deleteFood({})).to.throw("logId is required");
+    expect(() => UrlFactory.deleteFood({logId: null})).to.throw("logId is required");
+    done();
+  });
+
+  it('should throw provide correct url logId specified', done => {
+    const data = {
+      logId: 1517
+    };
+
+    expect(UrlFactory.deleteFood(data)).to.equal("https://api.fitbit.com/1/user/-/meals/1517.json");
+    done();
+  });
+
+});
